9e8704fb5c87201c9ec83d88950ffc8ea9cb1474
Shreedesh <nshreedesh@gmail.com>
2024-07-03 21:48:32 +0545

    update navbar and add router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="flex items-center justify-around pt-4 pb-4 bg-black text-white">
-      <div className="flex items-center">
-        <img src="https://themewagon.github.io/beko/img/logo.png" />
+      <div className="flex items-center cursor-pointer">
+        <img src="https://themewagon.github.io/beko/img/logo.png" alt="Logo" />
       </div>
-      <div className="flex items-center gap-10 tracking-wider">
-        <a href="#">Home</a>
-        <a href="#">Fighter</a>
-        <a href="#">Team</a>
-        <a href="#">Blog</a>
-        <a href="#">Contact</a>
+      <div className="flex items-center gap-10 tracking-wider cursor-pointer">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/fighter">Fighter</NavLink>
+        <NavLink to="/team">Team</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </div>
       <div>
         <button className="p-3 pl-5 pr-5 cursor-pointer bg-gradient-to-r from-red-600 to-pink-500 font-bold">
---
+++ src/pages/Fighter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Fighter() {
+  return <div>Fighter</div>;
+}
+
+export default Fighter;
