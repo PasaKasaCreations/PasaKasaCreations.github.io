bf2a2c5aee541160b7aebc66edd812b676ed08c9
Shreedesh <nshreedesh@gmail.com>
2024-07-07 21:34:05 +0545

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,11 +33,36 @@ function Navbar() {
         <img className="w-28" src="../logo.png" alt="Logo" />
       </div>
       <div className="flex items-center gap-10 tracking-wider cursor-pointer">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/fighter">Fighter</NavLink>
-        <NavLink to="/team">Team</NavLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+        <NavLink
+          to="/"
+          className="aria-[current=page]:underline underline-offset-4 color decoration-red-600"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className="aria-[current=page]:underline underline-offset-4 color decoration-red-600"
+          to="/fighter"
+        >
+          Fighter
+        </NavLink>
+        <NavLink
+          className="aria-[current=page]:underline underline-offset-4 color decoration-red-600"
+          to="/team"
+        >
+          Team
+        </NavLink>
+        <NavLink
+          className="aria-[current=page]:underline underline-offset-4 color decoration-red-600"
+          to="/blog"
+        >
+          Blog
+        </NavLink>
+        <NavLink
+          className="aria-[current=page]:underline underline-offset-4 color decoration-red-600"
+          to="/contact"
+        >
+          Contact
+        </NavLink>
       </div>
       <div>
         <button className="p-5 pl-10 pr-10 cursor-pointer bg-gradient-to-r from-red-600 to-pink-500 font-bold">
