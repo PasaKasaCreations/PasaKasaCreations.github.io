54b196e3540ec374d9f687d6bf6166e4d8d4de51
Shreedesh <nshreedesh@gmail.com>
2024-07-10 22:55:24 +0545

    update contact fighter page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="h-screen">
+        <div className="relative w-screen h-3/4">
+          <img
+            src="https://themewagon.github.io/beko/img/breadcrumb.png"
+            alt="Logo"
+            className="h-full w-full object-cover"
+          />
+          <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-Aclonica text-8xl">
+            Contact
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
