26db9f3db25451d2b1c90a7970f9a2568d9f5de8
Shreedesh <nshreedesh@gmail.com>
2024-08-07 09:58:56 +0545

    update service

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { TbActivityHeartbeat } from "react-icons/tb";
 
 function ServiceCard() {
   return (
     <div className="flex flex-col bg-white gap-3 rounded-sm h-64 justify-center px-5">
       <img src="" alt="" />
+      <TbActivityHeartbeat size={50} className="text-blue-500" />
       <p>Lorem Ipsum</p>
       <p>
         Voluptatum deleniti atque corrupti quos dolores et quas molestias
