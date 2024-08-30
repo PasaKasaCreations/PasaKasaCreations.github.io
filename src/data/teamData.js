59c0adee47625ec64fa778bb066aaaedce8befae
Shreedesh <nshreedesh@gmail.com>
2024-08-30 19:47:28 +0545

    add image

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -5,7 +5,7 @@ const teamData = [
     position: "CEO & System Designer",
     description: "Explicabo voluptatem mollitia et repellat qui dolorum quasi",
     imageUrl:
-      "https://bootstrapmade.com/demo/templates/Arsha/assets/img/team/team-1.jpg",
+      "https://media.licdn.com/dms/image/v2/D5603AQHDZGYKdBw4Ow/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1721496884205?e=1730332800&v=beta&t=4ApcjsH2I4T1BPFkl4nM78HxKUTfL2_FtDWMGio-ytw",
   },
   {
     id: 2,
@@ -14,7 +14,7 @@ const teamData = [
     description:
       "Aut maiores voluptates amet et quis praesentium qui senda para",
     imageUrl:
-      "https://bootstrapmade.com/demo/templates/Arsha/assets/img/team/team-2.jpg",
+      "https://media.licdn.com/dms/image/v2/D4E03AQH_I4fBchwsTA/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1672035155381?e=1730332800&v=beta&t=-Jae00CtisROjmEpMSNm-w1VZVF-JRvpASRe07WNH60",
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ const teamData = [
     position: "Creative Head & Story Writer",
     description: "Quisquam facilis cum velit laborum corrupti fuga rerum quia",
     imageUrl:
-      "https://bootstrapmade.com/demo/templates/Arsha/assets/img/team/team-3.jpg",
+      "https://media.licdn.com/dms/image/v2/D4D03AQGAD5GK7Q15kA/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1682839493034?e=1730332800&v=beta&t=DII0ipz2Du62e3puP8Yh9oudscWtAozXDKQRRMdEfo8",
   },
   {
     id: 4,
@@ -31,7 +31,7 @@ const teamData = [
     description:
       "Dolorum tempora officiis odit laborum officiis et et accusamus",
     imageUrl:
-      "https://bootstrapmade.com/demo/templates/Arsha/assets/img/team/team-4.jpg",
+      "https://media.licdn.com/dms/image/v2/D5603AQF1Mg17_G8BHw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1714579292176?e=1730332800&v=beta&t=HqSk5CNWvvFEbhX0XqPFUGwi8MA_Z4rCYTtj5mIQGTc",
   },
   {
     id: 5,
@@ -40,7 +40,7 @@ const teamData = [
     description:
       "Dolorum tempora officiis odit laborum officiis et et accusamus",
     imageUrl:
-      "https://bootstrapmade.com/demo/templates/Arsha/assets/img/team/team-4.jpg",
+      "https://media.licdn.com/dms/image/v2/C5603AQHnf22DF5832g/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1658745440185?e=1730332800&v=beta&t=ItTz2lFV_S947pzJugxmWZjh3embc-5fhEexvZKnyGk",
   },
   {
     id: 6,
@@ -49,7 +49,7 @@ const teamData = [
     description:
       "Dolorum tempora officiis odit laborum officiis et et accusamus",
     imageUrl:
-      "https://bootstrapmade.com/demo/templates/Arsha/assets/img/team/team-4.jpg",
+      "https://media.licdn.com/dms/image/v2/C4D03AQFyl770bhSucg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1654420945147?e=1730332800&v=beta&t=zfJy-450FVCLE4Eo7zgiffe3QddmLPU4y7AQ0J1U1qc",
   },
   {
     id: 7,
@@ -58,7 +58,7 @@ const teamData = [
     description:
       "Dolorum tempora officiis odit laborum officiis et et accusamus",
     imageUrl:
-      "https://bootstrapmade.com/demo/templates/Arsha/assets/img/team/team-4.jpg",
+      "https://media.licdn.com/dms/image/v2/C5603AQF6zEc1RFQr0w/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1643179356059?e=1730332800&v=beta&t=nUrxlSW0PaEjdzFn5A63nrJcOiskqx9bVNIffcaSARo",
   },
   {
     id: 8,
